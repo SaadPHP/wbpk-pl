9b4ff0135f3f09695df713afb0d942b4fa7d83be
Muhammad Saad <syedmuhammadsaad@live.com>
2020-12-01 20:33:08 +0500

    added multiple script files, added webpack confg file and set up bundle.js file in dist folder

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    // defining entry point
+    entry: "./assets/script1.js",
+
+    // defining an output file
+    output: {
+        path: `${__dirname}/dist`,
+        filename: "bundle.js"
+    }
+}
